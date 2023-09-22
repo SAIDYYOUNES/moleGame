e500aab6f086832a3b78740e2f7bd6c70bd05a47
saidyyounes <younes.saidy63@gmaail.com>
2023-09-22 23:41:35 +0100

    feed

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ var desiredSrc = "/img/hungry.png";
 function moleTransition(rand) {
     current.push(rand);
     var hole = document.querySelector("#hole-" + rand+" img");
-    hole.addEventListener("click", function() {
+    hole.parentNode.addEventListener("click", function() {
         // console.log(new URL(hole.src).pathname);
         if(new URL(hole.src).pathname === desiredSrc){
-            console.log('hole.src');
-            feedMole();
-            return;
+            var section = document.querySelector("#cursor");
+            clearInterval(intervalId);
+            feedMole(rand);
+            section.classList.remove('costum-cursor')
+            section.classList.add('cursor-worm')
+            setTimeout(function() {
+                section.classList.remove('cursor-worm')
+                section.classList.add('costum-cursor')
+              }, 800);
+            
         }
       });
     var images = ["","./img/hungry.png","./img/sad.png", "./img/leaving.png" ,""];
@@ -47,6 +54,18 @@ window.addEventListener("beforeunload", function () {
     clearInterval(intervalId);
 });
 
-function feedMole(){
-console.log('mnamnam');
+function feedMole(rand){
+// console.log('mnamnam');
+var hole = document.querySelector("#hole-" + rand+" img");
+
+var images = ["./img/fed.png", "./img/leaving.png" ,""];
+var currentIndex = 0;
+var intervalId = setInterval(function() {
+    hole.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+     if (currentIndex === 0) {
+clearInterval(intervalId);
+
+    }
+}, 800); 
 }
